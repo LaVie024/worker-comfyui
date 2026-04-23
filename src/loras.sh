f894b3f9578ce294188867111bb30ff0c0404e56
LaVie024 <62406970+LaVie024@users.noreply.github.com>
2026-04-23 18:33:12 +0000

    Add files via upload

---
+++ src/loras.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd /comfyui/models/loras
+wget https://huggingface.co/imspidey/LOallLora/resolve/main/Lora/2dRealPhoto_Background.safetensors
+wget https://huggingface.co/LeFeujitif/sandbox/resolve/main/Loras2/748cm_c_illu.safetensors
+wget https://huggingface.co/minaiosu/Shiiro0/resolve/main/HassaKu_style.safetensors
+wget https://huggingface.co/minaiosu/kot2/resolve/main/Matte_Skin_Illustrious_v4.safetensors
+wget https://huggingface.co/lopi999/test/resolve/main/NoctFlatStyleNoobAIVpredV3.safetensors
+wget https://huggingface.co/minaiosu/Shiiro0/resolve/main/Sinozick_style-.safetensors -O Sinozick_style.safetensors
+wget https://huggingface.co/nyaa314/lora/resolve/main/sliders/Smooth_Booster_v3.safetensors
+wget https://huggingface.co/lopi999/test/resolve/main/cryostyle.safetensors
+wget https://huggingface.co/minaiosu/ofake/resolve/main/cunnyfunkystyle2.safetensors
+wget https://huggingface.co/minaiosu/MrLoreTaker/resolve/main/flatline_v2_-_Illus_x_Niji.safetensors
+wget https://huggingface.co/itterative/misc-sdxl/resolve/main/loras/noob/jp06_v4.safetensors
+wget https://huggingface.co/Kutches/IL-ProV4/resolve/main/lighting_v2.safetensors
+wget https://huggingface.co/itterative/misc-sdxl/resolve/main/loras/noob/lolboja_v2.safetensors
+wget https://huggingface.co/lopi999/test/resolve/main/mon_monmon2133.safetensors
+wget https://huggingface.co/lopi999/test/resolve/main/nonchalant_NAI-XL.safetensors
+wget https://huggingface.co/lopi999/test/resolve/main/noobai_vp10_stabilizer_v0.280a_fp16.safetensors
+wget https://huggingface.co/ttttdiva/CivitAI_Auto12/resolve/main/noobai_vpred_1_masterpieces_v23/old_versions/noobai_vpred_1_masterpieces_v1.safetensors -O noobai_vpred_1_masterpieces.safetensors
+wget https://huggingface.co/chuyuekayo/ModelsCollections/resolve/main/oil_painting.safetensors
+wget https://huggingface.co/itterative/misc-sdxl/resolve/main/loras/noob/skuddbutt_v4.safetensors
+wget https://huggingface.co/itterative/misc-sdxl/resolve/main/loras/noob/wa_noob_13.safetensors
